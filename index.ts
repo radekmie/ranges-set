d14cd6ebc8bcec2b6478fb51154f75da1baf004d
Radosław Miernik <radekmie@gmail.com>
2024-04-13 13:50:30 +0200

    Inlined _kind.

--- index.ts
+++ index.ts
@@ -1,17 +1,12 @@
-const enum Kind {
-  Literal = 0,
-  Range = 1,
-}
-
 export type Repr = Literal | Range;
 
 export type Literal = {
-  /** @internal */ _kind: Kind.Literal;
+  /** @internal */ _isLiteral: 1;
   /** @internal */ _text: string;
 };
 
 export type Range = {
-  /** @internal */ _kind: Kind.Range;
+  /** @internal */ _isLiteral: 0;
   /** @internal */ _min: number;
   /** @internal */ _max: number;
 };
@@ -20,30 +15,29 @@ const numberPattern = /^(?:0|[1-9]\d*)$/;
 const rangePattern = /^(0|[1-9]\d*)\s*-\s*(0|[1-9]\d*)$/;
 
 export function _compare(reprA: Repr, reprB: Repr): number {
-  if (reprA._kind !== reprB._kind) {
-    return reprA._kind - reprB._kind;
+  if (reprA._isLiteral !== reprB._isLiteral) {
+    return reprB._isLiteral - reprA._isLiteral;
   }
 
-  switch (reprA._kind) {
-    case Kind.Literal:
-      return reprA._text >= (reprB as Literal)._text
-        ? reprA._text > (reprB as Literal)._text
-          ? 1
-          : 0
-        : -1;
-    case Kind.Range:
-      return (
-        reprA._min - (reprB as Range)._min || reprA._max - (reprB as Range)._max
-      );
+  if (reprA._isLiteral) {
+    return reprA._text >= (reprB as Literal)._text
+      ? reprA._text > (reprB as Literal)._text
+        ? 1
+        : 0
+      : -1;
   }
+
+  return (
+    reprA._min - (reprB as Range)._min || reprA._max - (reprB as Range)._max
+  );
 }
 
 export function _createLiteral(text: string): Repr {
-  return { _kind: Kind.Literal, _text: text };
+  return { _isLiteral: 1, _text: text };
 }
 
 export function _createRange(min: number, max: number): Repr {
-  return { _kind: Kind.Range, _min: min, _max: max };
+  return { _isLiteral: 0, _min: min, _max: max };
 }
 
 export function difference(textA: string, textB: string): string {
@@ -58,53 +52,48 @@ export function _differenceReprs(reprsA: Repr[], reprsB: Repr[]): Repr[] {
   const reprs: Repr[] = [];
   loop: for (let indexA = 0; indexA < reprsA.length; ++indexA) {
     const reprA = reprsA[indexA];
-    switch (reprA._kind) {
-      case Kind.Literal:
-        for (let indexB = 0; indexB < reprsB.length; ++indexB) {
-          const reprB = reprsB[indexB];
-          if (reprB._kind === Kind.Literal && reprB._text === reprA._text) {
+    if (reprA._isLiteral) {
+      for (let indexB = 0; indexB < reprsB.length; ++indexB) {
+        const reprB = reprsB[indexB];
+        if (reprB._isLiteral && reprB._text === reprA._text) {
+          continue loop;
+        }
+      }
+    } else {
+      for (let indexB = 0; indexB < reprsB.length; ++indexB) {
+        const reprB = reprsB[indexB];
+        if (!reprB._isLiteral) {
+          if (reprA._min >= reprB._min && reprA._max <= reprB._max) {
             continue loop;
           }
-        }
 
-        break;
-      case Kind.Range:
-        for (let indexB = 0; indexB < reprsB.length; ++indexB) {
-          const reprB = reprsB[indexB];
-          if (reprB._kind === Kind.Range) {
-            if (reprA._min >= reprB._min && reprA._max <= reprB._max) {
-              continue loop;
+          if (reprA._min <= reprB._min && reprA._max >= reprB._max) {
+            if (reprA._max > reprB._max) {
+              reprsA.splice(
+                indexA + 1,
+                0,
+                _createRange(reprB._max + 1, reprA._max),
+              );
             }
 
-            if (reprA._min <= reprB._min && reprA._max >= reprB._max) {
-              if (reprA._max > reprB._max) {
-                reprsA.splice(
-                  indexA + 1,
-                  0,
-                  _createRange(reprB._max + 1, reprA._max),
-                );
-              }
-
-              if (reprA._min < reprB._min) {
-                reprsA.splice(
-                  indexA + 1,
-                  0,
-                  _createRange(reprA._min, reprB._min - 1),
-                );
-              }
-
-              continue loop;
+            if (reprA._min < reprB._min) {
+              reprsA.splice(
+                indexA + 1,
+                0,
+                _createRange(reprA._min, reprB._min - 1),
+              );
             }
 
-            if (reprA._min >= reprB._min && reprA._min <= reprB._max) {
-              reprA._min = reprB._max + 1;
-            } else if (reprA._max >= reprB._min && reprA._max <= reprB._max) {
-              reprA._max = reprB._min - 1;
-            }
+            continue loop;
+          }
+
+          if (reprA._min >= reprB._min && reprA._min <= reprB._max) {
+            reprA._min = reprB._max + 1;
+          } else if (reprA._max >= reprB._min && reprA._max <= reprB._max) {
+            reprA._max = reprB._min - 1;
           }
         }
-
-        break;
+      }
     }
 
     reprs.push(reprA);
@@ -142,15 +131,11 @@ export function _expandReprs(reprs: Repr[]): string[] {
   const texts: string[] = [];
   for (let index = 0; index < reprs.length; ++index) {
     const repr = reprs[index];
-    switch (repr._kind) {
-      case Kind.Literal:
-        texts.push(repr._text);
-        break;
-      case Kind.Range: {
-        for (let index = repr._min; index <= repr._max; ++index) {
-          texts.push(`${index}`);
-        }
-        break;
+    if (repr._isLiteral) {
+      texts.push(repr._text);
+    } else {
+      for (let index = repr._min; index <= repr._max; ++index) {
+        texts.push(`${index}`);
       }
     }
   }
@@ -166,19 +151,17 @@ export function intersection(textA: string, textB: string): string {
 }
 
 export function _intersectionRepr(reprA: Repr, reprB: Repr): Repr | null {
-  if (reprA._kind !== reprB._kind) {
+  if (reprA._isLiteral !== reprB._isLiteral) {
     return null;
   }
 
-  switch (reprA._kind) {
-    case Kind.Literal:
-      return reprA._text === (reprB as Literal)._text ? reprA : null;
-    case Kind.Range: {
-      const min = Math.max(reprA._min, (reprB as Range)._min);
-      const max = Math.min(reprA._max, (reprB as Range)._max);
-      return min > max ? null : _createRange(min, max);
-    }
+  if (reprA._isLiteral) {
+    return reprA._text === (reprB as Literal)._text ? reprA : null;
   }
+
+  const min = Math.max(reprA._min, (reprB as Range)._min);
+  const max = Math.min(reprA._max, (reprB as Range)._max);
+  return min > max ? null : _createRange(min, max);
 }
 
 export function _intersectionReprs(reprsA: Repr[], reprsB: Repr[]): Repr[] {
@@ -237,14 +220,11 @@ export function _serialize(reprs: Repr[]): string {
 }
 
 export function _serializeOne(repr: Repr): string {
-  switch (repr._kind) {
-    case Kind.Literal:
-      return repr._text;
-    case Kind.Range:
-      return repr._min === repr._max
-        ? `${repr._min}`
-        : `${repr._min}-${repr._max}`;
+  if (repr._isLiteral) {
+    return repr._text;
   }
+
+  return repr._min === repr._max ? `${repr._min}` : `${repr._min}-${repr._max}`;
 }
 
 export function subset(textA: string, textB: string): boolean {
@@ -256,30 +236,27 @@ export function subset(textA: string, textB: string): boolean {
 export function _subsetReprs(reprsA: Repr[], reprsB: Repr[]): boolean {
   loop: for (let indexB = 0; indexB < reprsB.length; ++indexB) {
     const reprB = reprsB[indexB];
-    switch (reprB._kind) {
-      case Kind.Literal:
-        for (let indexA = 0; indexA < reprsA.length; ++indexA) {
-          const reprA = reprsA[indexA];
-          if (reprA._kind === Kind.Literal && reprA._text === reprB._text) {
-            continue loop;
-          }
+    if (reprB._isLiteral) {
+      for (let indexA = 0; indexA < reprsA.length; ++indexA) {
+        const reprA = reprsA[indexA];
+        if (reprA._isLiteral && reprA._text === reprB._text) {
+          continue loop;
         }
-
-        return false;
-      case Kind.Range:
-        for (let indexA = 0; indexA < reprsA.length; ++indexA) {
-          const reprA = reprsA[indexA];
-          if (
-            reprA._kind === Kind.Range &&
-            reprA._min <= reprB._min &&
-            reprA._max >= reprB._max
-          ) {
-            continue loop;
-          }
+      }
+    } else {
+      for (let indexA = 0; indexA < reprsA.length; ++indexA) {
+        const reprA = reprsA[indexA];
+        if (
+          !reprA._isLiteral &&
+          reprA._min <= reprB._min &&
+          reprA._max >= reprB._max
+        ) {
+          continue loop;
         }
-
-        return false;
+      }
     }
+
+    return false;
   }
 
   return true;
@@ -291,30 +268,27 @@ export function union(textA: string, textB: string): string {
 }
 
 export function _unionRepr(reprA: Repr, reprB: Repr): boolean {
-  if (reprA._kind !== reprB._kind) {
+  if (reprA._isLiteral !== reprB._isLiteral) {
     return false;
   }
 
-  switch (reprA._kind) {
-    case Kind.Literal: {
-      const same = reprA._text === (reprB as Literal)._text;
-      return same;
-    }
-    case Kind.Range: {
-      const unionable =
-        (reprA._min <= (reprB as Range)._max + 1 &&
-          reprA._max >= (reprB as Range)._min) ||
-        ((reprB as Range)._min <= reprA._max + 1 &&
-          (reprB as Range)._max >= reprA._min);
-
-      if (unionable) {
-        reprA._min = Math.min(reprA._min, (reprB as Range)._min);
-        reprA._max = Math.max(reprA._max, (reprB as Range)._max);
-      }
-
-      return unionable;
-    }
+  if (reprA._isLiteral) {
+    const same = reprA._text === (reprB as Literal)._text;
+    return same;
   }
+
+  const unionable =
+    (reprA._min <= (reprB as Range)._max + 1 &&
+      reprA._max >= (reprB as Range)._min) ||
+    ((reprB as Range)._min <= reprA._max + 1 &&
+      (reprB as Range)._max >= reprA._min);
+
+  if (unionable) {
+    reprA._min = Math.min(reprA._min, (reprB as Range)._min);
+    reprA._max = Math.max(reprA._max, (reprB as Range)._max);
+  }
+
+  return unionable;
 }
 
 export function _unionReprs(reprs: Repr[], repr: Repr): void {
